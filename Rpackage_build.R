0d5e7cd510d8d57749b76c5da983902209931f5c
Michael Sullivan <m.r.sullivan@stanford.edu>
2016-08-04 18:48:33 -0700

    Altered R package build script

--- Rpackage_build.R
+++ Rpackage_build.R
@@ -7,23 +7,16 @@ Main <- function(){
     CRAN_result     <- lapply(CRAN_packages, install_CRAN, repo = "http://cran.cnr.Berkeley.edu/", 
                               dependency = TRUE, quiet = TRUE)
     
-    GitHub_packages <- c("gslab-econ/gslab_r/loadpaths")
+    library(devtools)
     
-    GitHub_result   <- lapply(GitHub_packages, install_GitHub, quiet = TRUE)
+    GitHub_packages <- c("loadpaths")
+    
+    GitHub_result   <- lapply(GitHub_packages, function(pkg) install_github('gslab_econ/gslab_r', subdir = pkg))
 }
 
 install_CRAN <- function(pkg, repo, dependency, quiet){
     if (system.file(package = pkg) == "") {
         install.packages(pkg, repos = repo, dependencies = dependency, quiet = quiet)
-    }
-}
-
-install_GitHub <- function(pkg, quiet) {
-    pkg_loc  <- nchar(pkg) - regexpr("\\/[^\\/]*$", pkg)
-    pkg_name <- substr(pkg, (nchar(pkg) + 1) - pkg_loc, nchar(pkg))
-    
-    if (system.file(package = pkg_name) == "") {
-        install.packages(pkg, quiet = quiet)
     }
 }
 
