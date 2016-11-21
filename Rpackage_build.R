b78ac92df984d1f91c6a78a1288185cc1cc6cdac
Michael Sullivan <m.r.sullivan@stanford.edu>
2016-11-21 13:59:26 -0800

    Update Rpackage_build.R

--- Rpackage_build.R
+++ Rpackage_build.R
@@ -1,14 +1,17 @@
 
 Main <- function(){
+    library(devtools)
+    # Update all currently installed packages
+    update.packages(ask = FALSE, checkBuilt = TRUE, repos = "http://cran.cnr.Berkeley.edu/")
     
+    # Install packages from CRAN
     CRAN_packages   <- c("data.table", "devtools",   "foreign",   "ggplot2", 
                          "Matrix",     "parallel",   "plyr",      "readstata13",
                          "reshape2",   "roxygen2",   "RUnit",     "testthat")  
-    
     CRAN_result     <- lapply(CRAN_packages, install_CRAN, repo = "http://cran.cnr.Berkeley.edu/", 
                               dependency = TRUE, quiet = TRUE)
     
-    library(devtools)
+    # Install packages from GitHub
     GitHub_packages <- c("loadpaths")
     GitHub_result   <- lapply(GitHub_packages, function(pkg) install_github('gslab-econ/gslab_r', subdir = pkg))
 }
