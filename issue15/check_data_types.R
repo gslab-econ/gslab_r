749ad6b73ac1fcdf61f6d22a1951576f5c2dc5df
Veli Murat ANDIRIN <62054739+veli-m-andirin@users.noreply.github.com>
2022-04-10 11:49:49 -0400

    #15 modifies test script

--- issue15/check_data_types.R
+++ issue15/check_data_types.R
@@ -1,6 +1,13 @@
-df_1 <- data.frame("a" = runif(10000, 0, 1), "b" = rnorm(10000, 5, 2),
-                   "c" = sample(c("x", "y", "z"), 10000, replace = T),
-                   id = 1:10000)
+remove(list=ls())
+library(SaveData)
+
+set.seed(0)
+
+n <- 10000
+df_1 <- data.frame("a" = round(runif(n, 0, 1), 2), 
+                   "b" = round(rnorm(n, 5, 2), 2),
+                   "c" = sample(c("x", "y", "z"), n, replace = T),
+                   id = 1:n)
 
 SaveData(df_1, key = "id", outfile = "issue15/df_1.csv",
          logfile = "issue15/df1_manifest.log")
@@ -13,5 +20,3 @@ SaveData(df_1, key = "id", outfile = "issue15/df_1.dta",
 
 SaveData(df_1, key = "id", outfile = "issue15/df_1.RData",
          logfile = "issue15/df1_manifest.log", appendlog = T)
-
-
