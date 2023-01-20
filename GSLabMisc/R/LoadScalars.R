1be2c4984f458f30d9efd431ba86e05493bca87c
ew487 <108754119+ew487@users.noreply.github.com>
2023-01-20 15:27:56 -0500

    #25 Add LoadScalars() to GSLabMisc

---
+++ GSLabMisc/R/LoadScalars.R
@@ -0,0 +1,55 @@
+#' Load scalars from a text file into global environment.
+#'
+#' @param input path to `.txt` file.
+#' @param where where to load the scalars. Defaults to 1 to load to the global environment.
+#' @param convert whether to guess data type. Defaults to `TRUE`. If `FALSE`, stores values as strings.
+#' @param ... additional arguments to `utils::type.convert`.
+#' 
+#' @importFrom stringr str_split
+#' @importFrom utils type.convert
+#' @export
+#' 
+
+LoadScalars <- function(input, where = 1, convert = TRUE, ...) {
+  
+  stopifnot("input must be a `.txt` file" = grepl("\\.txt$", input))
+  con <- file(input, "r")
+  
+  while (TRUE) {
+    
+    line = readLines(con, n = 1)
+    if (length(line) == 0) {
+      break
+    }
+    
+    line_vec <- stringr::str_split(trimws(line), " ", n = 2)[[1]]
+    if (!startsWith(line_vec[1], "#") & length(line_vec) == 2) {
+      
+      name <- make.names(line_vec[1])
+      if (name != line_vec[1]) {
+        
+        warning(sprintf("`%s' is an invalid name, skipping", line_vec[1]))
+        
+      } else if (exists(name, where = .GlobalEnv)) {
+        
+        warning(sprintf("`%s' already exists, skipping", line_vec[1]))
+        
+        } else {
+        
+        value <- scan(text = line_vec[2], what = "character", quiet = TRUE)
+        if (convert) {
+          
+          if ("as.is" %in% names(list(...))) {
+            value <- utils::type.convert(value, ...)
+            
+          } else {
+            value <- utils::type.convert(value, as.is = TRUE, ...)
+          }
+        }
+        
+        assign(name, value, envir = as.environment(where))
+      }
+    }
+  }
+  close(con)
+}
--- GSLabMisc/README.md
+++ GSLabMisc/README.md
@@ -6,3 +6,6 @@ GSLabMisc() package provides some helper functions.
 
 ## AutoFill
 Use `AutoFill()` to save TeX macros as described [here](https://github.com/gslab-econ/ra-manual/wiki/Autofilling-Values/4a41786eed04d627060f6c5de37f6fc304ef6025#in-text-values).
+
+## LoadScalars
+Use `LoadScalars()` to load values from a .txt file into the global environment. Each line of the input file is assumed to have the name of the global variable followed by a space followed by the value of the global variable. Lines beginning with # are treated as comments. Lines in the file that do not have at least two words (defined as blocks of text separated by a space) are ignored. Strings enclosed in double quotes count as one word.
