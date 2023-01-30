a3848479dd05f87ef4f384c8cfffe06c9c751c14
ew487 <108754119+ew487@users.noreply.github.com>
2023-01-30 12:26:44 -0500

    #25 Fix notes for Autofill

--- GSLabMisc/R/AutoFill.R
+++ GSLabMisc/R/AutoFill.R
@@ -6,19 +6,22 @@
 #' @param mode If specified as "text", will appear as normal text even in math mode. If this is undesirable, use "math".
 #' @param appendmode If TRUE, appends to an existing file.
 #'
+#' @importFrom utils write.table capture.output
 #' @export
 #' @examples
+#' \dontrun{
 #' AutoFill(commandname = "pValue", content = "0.0003", "pvalue.tex", mode = "math")
-#'
-#' #If you would like to export in text mode:
+#' 
+#' # If you would like to export in text mode:
 #' AutoFill(commandname = "SampleStart", content = "January 2010", "samplestart.tex", mode = "text")
+#' }
 
 AutoFill <- function(commandname, content, outfile, mode = "math", appendmode = FALSE) {
     if (mode == "text") {
-        write.table(capture.output(cat(sprintf("\\newcommand{\\%s}{\\textnormal{%s}}", commandname, content))),
+        utils::write.table(utils::capture.output(cat(sprintf("\\newcommand{\\%s}{\\textnormal{%s}}", commandname, content))),
                     outfile, quote = F, col.names = F, row.names = F, append = appendmode)
     } else if (mode == "math") {
-        write.table(capture.output(cat(sprintf("\\newcommand{\\%s}{%s}", commandname, content))),
+        utils::write.table(utils::capture.output(cat(sprintf("\\newcommand{\\%s}{%s}", commandname, content))),
                     outfile, quote = F, col.names = F, row.names = F, append = appendmode)
     }
 }
