78920ff5bca8862f8bf262f9e2f89ad7df30fe24
zhizhongpu <84325421+zhizhongpu@users.noreply.github.com>
2024-07-11 18:30:46 -0400

    #29 fx bugs

--- SaveData/R/SaveData.R
+++ SaveData/R/SaveData.R
@@ -85,7 +85,7 @@ SaveData <- function(df, key, outfile, logfile = NULL, appendlog = FALSE, sortby
     return(list("outfile" = outfile, "logfile" = logfile, "filetype" = filetype))
   }
   
-  CheckKey <- function(df, key, colname_order = reordered_colnames) {        
+  CheckKey <- function(df, key, colname_order) {        
     
     if (!all(key %in% colnames(df))) {
       
@@ -109,46 +109,45 @@ SaveData <- function(df, key, outfile, logfile = NULL, appendlog = FALSE, sortby
       
       if (sortbykey) {
         setorderv(df, key)  # sort by key values
-      }           
+      }
       
-      setcolorder(df, reordered_colnames)
+      setcolorder(df, colname_order)
     }
   }
   
   
-  WriteLog <- function(df, key, outfile, logfile = NULL, appendlog = TRUE,
-                       colname_order = reordered_colnames) {
+  WriteLog <- function(df, key, outfile, logfile = NULL, appendlog = TRUE) {
+    
+    reordered_colnames = names(df)
     
     if (logfile == FALSE) return(NULL)
     
-    numeric_cols <- reordered_colnames[sapply(df, is.numeric)]
-    non_numeric_cols <- setdiff(colname_order, numeric_cols)
+    numeric_cols <- reordered_colnames[sapply(df, FUN = is.numeric)]
+    non_numeric_cols <- setdiff(reordered_colnames, numeric_cols)
     
     numeric_sum <- df[, .(
-      variable_name = colname_order,
-      mean = lapply(.SD, mean, na.rm = TRUE),
-      sd = lapply(.SD, sd, na.rm = TRUE),
-      min = lapply(.SD, min, na.rm = TRUE),
-      max = lapply(.SD, max, na.rm = TRUE)
+      variable_name = numeric_cols,
+      mean = lapply(.SD, mean, na.rm = T),
+      sd = lapply(.SD, sd, na.rm = T),
+      min = lapply(.SD, min, na.rm = T),
+      max = lapply(.SD, max, na.rm = T)
     ), .SDcols = numeric_cols]
     
     non_numeric_sum <- df[, .(
-      variable_name = colname_order,
+      variable_name = non_numeric_cols,
       uniqueN = lapply(.SD, function(x) uniqueN(x))
     ), .SDcols = non_numeric_cols]
     
     all_sum <- df[, .(
-      variable_name = colname_order,
+      variable_name = reordered_colnames,
       type = sapply(.SD, class),
       N = lapply(.SD, function(x) sum(!is.na(x)))
     )]
     
     sum <- all_sum |> 
-      merge(non_numeric_sum, by = "variable_name", all.x = T) |> 
-      merge(numeric_sum, by = "variable_name", all.x = T)
-      
-    
-    sum <- sum[match(colname_order, sum$variable_name),]
+      merge(non_numeric_sum, by = "variable_name", all.x = TRUE) |> 
+      merge(numeric_sum, by = "variable_name", all.x = TRUE) |> 
+      arrange(match(variable_name, reordered_colnames))
     
     hash <- digest(df, algo="md5")
     
@@ -174,7 +173,7 @@ SaveData <- function(df, key, outfile, logfile = NULL, appendlog = FALSE, sortby
   h <- DataDictionary()
   files <- CheckExtension(outfile, h, logfile)
   
-  CheckKey(df, key)
+  CheckKey(df, key, colname_order = reordered_colnames)
   WriteLog(df, key, files$outfile, files$logfile, appendlog)
   WriteData(df, files$outfile, files$filetype, h)
   
