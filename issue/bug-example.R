42edd65192a3b00766b4f8f6f14ac3c86ca0d137
Chris Liao <33707455+liaochris@users.noreply.github.com>
2026-02-10 09:53:00 -0500

    add details on non vector #47

--- issue/bug-example.R
+++ issue/bug-example.R
@@ -22,3 +22,24 @@ SaveData(out2, key = "year",
          outfile = "issue/out2.csv",
          logfile = F,
          appendlog = T)
+
+
+dt_test2 <- data.frame(
+  year = 3:1,
+  rmse = 3:1,
+  model_vars = "model vars"
+)
+
+out3 <- dt_test2 %>% dplyr::select(-c(rmse))
+out4 <- dt_test2 %>% dplyr::select(year, rmse)
+print(out4)
+SaveData(out3 %>% dplyr::select(-model_vars),
+         key = "year",
+         outfile = "issue/out3.csv",
+         logfile = F,
+         appendlog = T)
+print(out4)
+SaveData(out4, key = "year",
+         outfile = "issue/out4.csv",
+         logfile = F,
+         appendlog = T)
